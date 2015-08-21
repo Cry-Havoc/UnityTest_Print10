ab23fce5623f316e6cbf848bd0fc4194722c648f
Cry-Havoc <joergreisig@gmail.com>
2015-08-21 20:12:10 +0200

    Test Unity Project that simulates 10 Print

---
+++ Assets/TenPrinter.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+
+
+public class TenPrinter : MonoBehaviour {
+
+	GameObject screenTextField;
+
+	string outPutText;
+
+	public int rowLength = 50;
+	public int maxNumberOfRows = 26;
+	
+
+
+	// Use this for initialization
+	void Start () {
+
+		screenTextField = GameObject.Find ("OutputTextField");
+
+		outPutText = "";
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Random.Range (0, 2) == 0) {
+			outPutText = outPutText + "/";
+		}
+		else
+		{
+			outPutText = outPutText + "\\";
+		}
+
+		if (outPutText.Length <= rowLength * maxNumberOfRows) {
+		
+			screenTextField.GetComponent<Text>().text = outPutText; 
+			
+		}
+
+	}
+}
